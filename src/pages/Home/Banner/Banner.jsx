8c755f603e25c344667d50d98009f7cf58f98b3a
SumaiyaAK <sumaiyasrn6@gmail.com>
2023-06-09 17:18:12 +0600

    banner is ready

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,27 @@
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import banner from '../../../assets/pictures/npr.brightspotcdn.png'
+import banner2 from '../../../assets/pictures/milesTone 10.jpg'
+import banner3 from '../../../assets/pictures/languages3.png'
+
+const Banner = () => {
+    return (
+        <Carousel>
+                <div className="h-60">
+                    <img className="" src={banner} />
+                   
+                </div>
+                <div>
+                    <img src={banner2} />
+                    
+                </div>
+                <div>
+                    <img src={banner3} />
+                    
+                </div>
+            </Carousel>
+    );
+};
+
+export default Banner;
