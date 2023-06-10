49a6fc5bbbc466d361c6f03facce8883ca440c40
SumaiyaAK <sumaiyasrn6@gmail.com>
2023-06-10 11:36:25 +0600

    json data added and navabr, footer added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,26 +1,38 @@
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+
+
 import banner from '../../../assets/pictures/npr.brightspotcdn.png'
 import banner2 from '../../../assets/pictures/milesTone 10.jpg'
 import banner3 from '../../../assets/pictures/languages3.png'
+import banner4 from '../../../assets/pictures/lingo10.avif'
 
 const Banner = () => {
     return (
+        
         <Carousel>
-                <div className="h-60">
-                    <img className="" src={banner} />
-                   
-                </div>
-                <div>
-                    <img src={banner2} />
-                    
-                </div>
-                <div>
-                    <img src={banner3} />
-                    
-                </div>
-            </Carousel>
+            
+
+            <div>
+                <img src={banner4} />
+
+            </div>
+            <div className="h-60">
+                <img className="" src={banner} />
+
+            </div>
+            <div>
+                <img src={banner2} />
+
+            </div>
+
+            <div>
+                <img src={banner3} />
+
+            </div>
+        </Carousel>
+        
     );
 };
 
---
+++ src/pages/Home/PopularClasses/PopularClasses.jsx
@@ -0,0 +1,11 @@
+
+
+const PopularClasses = () => {
+    return (
+        <section>
+            <h2>Popular Classes</h2>
+        </section>
+    );
+};
+
+export default PopularClasses;
