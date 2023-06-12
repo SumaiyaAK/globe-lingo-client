cc5eedddcae5f56601da690c1a6b10ccf2d5a82d
SumaiyaAK <sumaiyasrn6@gmail.com>
2023-06-12 19:33:55 +0600

    Registration is activated

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 
 const Registration = () => {
-    const { signIn } = useContext(AuthContext)
+    const { createUser } = useContext(AuthContext)
 
 
 
@@ -14,12 +14,15 @@ const Registration = () => {
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
+        const ConfirmPassword = form.confirmPassword.value;
+        const PhotoUrl = form.photoUrl.value;
         console.log(email, password);
-        signIn(email, password)
+        createUser(email, password, ConfirmPassword, PhotoUrl)
             .then(result => {
                 const user = result.user;
                 console.log(user)
             })
+            .then(error => console.log(error))
     }
     return (
         <div>
@@ -34,6 +37,12 @@ const Registration = () => {
                         <div className="card-body">
                             <div className="form-control">
                                 <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name="name" placeholder="name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
                                 <input type="email" name="email" placeholder="email" className="input input-bordered" />
@@ -43,15 +52,27 @@ const Registration = () => {
                                     <span className="label-text">Password</span>
                                 </label>
                                 <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                                
+                            </div>
+                            <div className="form-control">
                                 <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    <span className="label-text">Confirm Password</span>
                                 </label>
+                                <input type="password" name="confirmPassword" placeholder="Confirm Password" className="input input-bordered" />
+                                
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo URL</span>
+                                </label>
+                                <input type="photo url" name="photoUrl" placeholder="Photo URL" className="input input-bordered" />
+                                
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <button className="btn btn-primary">Submit</button>
                             </div>
                         </div>
-                        <p className="p-4 text-center"><small>If you do not have an account <Link to="/registration">Create an account</Link></small></p>
+                        <p className="p-4 text-center"><small>If you already have an account <Link to="/login">Login</Link></small></p>
                     </form>
                     
                 </div>
